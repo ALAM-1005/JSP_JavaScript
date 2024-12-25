f5cf59aeb97ce0419980697439a2a72a6f98e0c4
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2024-12-25 04:05:56 +0000

    initial

---
+++ Day07/app.js
@@ -0,0 +1,41 @@
+let a = 10;
+let b = 'hello';
+
+console.log(`The sum of ${a} and ${b} is ${a+b}`)
+
+console.log(`The sum of ${a} and ${b} is ${a-b}`)
+
+
+//  for loop
+
+// for(let i =1;i<=a;i++){
+
+//     console.log(i)
+// }
+
+// while loop
+
+
+// console.log('*****WhileLoop*****')
+// let i  = 1
+// while(i<=20){
+
+//     console.log(i)
+
+//     i+=2
+// }
+
+
+// check the data type of variable to used `typeOf` operator
+
+console.log(typeof a)
+
+let student = {
+
+
+    name :'juned alam',
+    age :22,
+    id:1
+}
+
+console.log( typeof student)
