361e5293cf63acc050d96e2bfa1e852853a5081e
Md JUNED ALAM <93564319+ALAM-1005@users.noreply.github.com>
2025-01-09 16:39:59 +0000

    third ways of printing string using new keyword

--- js/app.js
+++ js/app.js
@@ -4,5 +4,10 @@ const rows = 10
 
 console.log(name+rows);
 
-console.log(`my name is ${name} and the number of rows is ${rows}`);
+console.log(`my name is ${name} and the number of rows is ${rows}`);  // most prefered
+
+
+const values = new String('juned alam')
+console.log(values.length);
+
 
